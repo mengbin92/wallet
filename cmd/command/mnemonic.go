b31a0a093041eee9a0d4594dec1757fbb000836f
mengbin92 <mengbin1992@outlook.com>
2024-08-27 15:05:13 +0800

    fix(增加注释):

--- cmd/command/mnemonic.go
+++ cmd/command/mnemonic.go
@@ -10,12 +10,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// mnemonic command
+// mnemonicCmd 方法定义了一个用于管理助记词的命令，该命令包含创建和加载助记词的子命令。
 func (c *WalletCommand) mnemonicCmd() *cobra.Command {
 	genMnemonicCmd := &cobra.Command{
 		Use:   "mnemonic",
-		Short: "Manage mnenomic",
-		Long:  "Manage mnenomic",
+		Short: "Manage mnemonic",
+		Long:  "Manage mnemonic",
 	}
 
 	genMnemonicCmd.AddCommand(c.createMnemonic())
@@ -25,68 +25,64 @@ func (c *WalletCommand) mnemonicCmd() *cobra.Command {
 	return genMnemonicCmd
 }
 
-// create new mnemonic command
+// createMnemonic 方法定义了一个创建新助记词的命令，该命令会生成一个新的助记词并保存到文件中。
 func (c *WalletCommand) createMnemonic() *cobra.Command {
-	createMnemonicCmd := &cobra.Command{
+	return &cobra.Command{
 		Use:   "create",
 		Short: "Create a new mnemonic and save it to file",
 		Long:  "Create a new mnemonic and save it to file, example: ./wallet mnemonic create ./mnemonic.txt password",
 		RunE:  c.runCreateMnemonic,
 	}
-	return createMnemonicCmd
 }
 
-// save mnemonic command
+// saveMnemonic 方法定义了一个保存助记词到文件的命令。
 func (c *WalletCommand) saveMnemonic() *cobra.Command {
-	saveMnemonicCmd := &cobra.Command{
+	return &cobra.Command{
 		Use:   "save",
 		Short: "Save a mnemonic to file",
 		Long:  "Save a mnemonic to file",
 		RunE:  c.runSaveMnemonic,
 	}
-	return saveMnemonicCmd
 }
 
-// load mnemonic command
+// loadMnemonic 方法定义了一个从文件加载助记词的命令。
 func (c *WalletCommand) loadMnemonic() *cobra.Command {
-	loadMnemonicCmd := &cobra.Command{
+	return &cobra.Command{
 		Use:   "load",
 		Short: "Load a mnemonic from file",
 		Long:  "Load a mnemonic from file",
 		RunE:  c.runLoadMnemonic,
 	}
-	return loadMnemonicCmd
 }
 
+// runCreateMnemonic 方法实现了创建新助记词的逻辑，包括生成助记词和保存助记词到文件。
 func (c *WalletCommand) runCreateMnemonic(cmd *cobra.Command, args []string) error {
 	fmt.Println("Create a new mnemonic")
-	if len(args) < 1{
+	if len(args) < 1 {
 		return errors.New("Please provide the file path to save the mnemonic, e.g. ./mnemonic.txt")
 	}
+
 	mnemonic, err := kms.GenMnemonic()
 	if err != nil {
 		return errors.Wrap(err, "generate mnemonic failed")
 	}
+
 	fmt.Println("Your new mnemonic is: ", mnemonic)
 	c.mnemonic = mnemonic
 
-	// save the mnemonic to file
-	c.runSaveMnemonic(cmd, args)
-	return nil
+	return c.runSaveMnemonic(cmd, args)
 }
 
+// runSaveMnemonic 方法实现了保存助记词到文件的逻辑，包括加密助记词和保存加密后的内容到文件。
 func (c *WalletCommand) runSaveMnemonic(cmd *cobra.Command, args []string) error {
-	fmt.Println("Save a mnemonic to file: ",c.mnemonic)
-	// args[0] is the file path
 	store := storage.NewLocalStorage(args[0])
-	// args[1] is the password
-	// if len(args) < 2, create a new password
 	var password string
 	var err error
-	if len(args) ==2{
+
+	if len(args) == 2 {
 		password = args[1]
-	} else{
-		password,err = utils.CreatePassphrase(12)
+	} else {
+		password, err = utils.CreatePassphrase(12)
 		if err != nil {
 			return errors.Wrap(err, "create password failed")
 		}
@@ -106,22 +102,24 @@ func (c *WalletCommand) runSaveMnemonic(cmd *cobra.Command, args []string) error
 	return nil
 }
 
+// runLoadMnemonic 方法实现了从文件加载助记词的逻辑，包括从文件读取加密的助记词和解密助记词。
 func (c *WalletCommand) runLoadMnemonic(cmd *cobra.Command, args []string) error {
-	fmt.Println("Load a mnemonic from file")
-	// args[0] is the file path
+	if len(args) < 2 {
+		return errors.New("Please provide the file path and password")
+	}
+
 	store := storage.NewLocalStorage(args[0])
-	// args[1] is the password
-	// load the encrypted mnemonic from file
-	encryptedMnemonic,err := store.Load()
-	if err != nil{
+	encryptedMnemonic, err := store.Load()
+	if err != nil {
 		return errors.Wrap(err, "load mnemonic failed")
 	}
 
 	// decrypt the mnemonic with password
 	mnemonic, err := utils.AesDecrypt(encryptedMnemonic, args[1])
-	if err !=nil{
+	if err != nil {
 		return errors.Wrap(err, "decrypt mnemonic failed")
 	}
+
 	c.mnemonic = string(mnemonic)
 	fmt.Println("Your mnemonic is: ", c.mnemonic)
 	return nil
